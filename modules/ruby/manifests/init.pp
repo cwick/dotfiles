c233401265f8ab5192e7ca4e8fc01301f8d3a865
Carmen Wick <CarmenWick@gmail.com>
2011-11-03 20:29:20 -0700

    Put rvm path into local variable

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,12 +1,14 @@
 class ruby {
+    $rvmdir = "/usr/share/ruby-rvm"
+
     exec { "rvm package install openssl":
       alias   => "rvm-openssl",
-      creates => "/usr/share/ruby-rvm/usr/lib/libssl.so",
+      creates => "${rvmdir}/usr/lib/libssl.so",
       require => [Package["libssl-dev"], Package["ruby-rvm"]],
     }
 
     exec { "rvm install 1.9.2-p180 --with-openssl-dir=/usr/share/ruby-rvm/usr":
-      creates => "/usr/share/ruby-rvm/rubies/ruby-1.9.2-p180/",
+      creates => "${rvmdir}/rubies/ruby-1.9.2-p180/",
       require => Exec["rvm-openssl"],
     }
 }
