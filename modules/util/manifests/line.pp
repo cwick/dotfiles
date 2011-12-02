c22976f2428c0059b4c60552455d91cf7535c9ca
Carmen Wick <CarmenWick@gmail.com>
2011-12-02 14:01:35 -0800

    This information might be useful later on

--- modules/util/manifests/line.pp
+++ modules/util/manifests/line.pp
@@ -11,5 +11,11 @@ define line($file, $line, $ensure = 'present') {
         onlyif => "/bin/grep -qFx '${line}' '${file}'"
       }
     }
+
+    # Use this resource instead if your platform's grep doesn't support -vFx;
+    # note that this command has been known to have problems with lines containing quotes.
+    # exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+    #     onlyif => "/bin/grep -qFx '${line}' '${file}'"
+    # }
   }
 }
