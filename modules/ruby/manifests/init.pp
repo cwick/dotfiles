7f7d322143faafdd2b1311f63d4e27d810285166
Carmen Wick <CarmenWick@gmail.com>
2012-03-22 12:11:14 -0700

    Don't use ruby openssl

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -13,23 +13,24 @@ class ruby {
     # Additional Ruby dependencies listed in 'rvm requirements'
     package { [build-essential, openssl, libreadline6, libreadline6-dev, curl, git-core, zlib1g, zlib1g-dev, libssl-dev, libyaml-dev, libsqlite3-0, libsqlite3-dev, libxml2-dev, libxslt1-dev, autoconf, libc6-dev, libncurses5-dev, automake, libtool, bison, subversion]:
     }
-    
+
     exec { "curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o rvm-install; chmod +x rvm-install; ./rvm-install":
       alias   => "rvm-install",
       require => Package["curl"],
       creates => $rvmdir
     }
-    
-    exec { "bash -c \"${rvm} pkg install openssl\"":
-      alias   => "rvm-openssl",
-      creates => "${rvmdir}/usr/lib/libssl.so",
-      require => Exec["rvm-install"],
-    }
+
+    # Don't think I need this anymore. Was causing segfaults in the ruby interpreter
+    # exec { "bash -c \"${rvm} pkg install openssl\"":
+    #   alias   => "rvm-openssl",
+    #   creates => "${rvmdir}/usr/lib/libssl.so",
+    #   require => Exec["rvm-install"],
+    # }
 
     # This exec rule used to use the option "--with-openssl-dir=${rvmdir}/usr"
     # But this caused a crash in the ruby interpreter when invoking PGconn.connect
     exec { "bash -c \"${rvm} install ${version}\"":
       creates => "${rvmdir}/rubies/ruby-${version}/",
-      require => Exec["rvm-openssl"],
+      #      require => Exec["rvm-openssl"],
     }
 }
